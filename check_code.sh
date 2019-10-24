366af96bf3e8d65164b4d9b98bee2bccb4bd24d6
Sacha Delanoue <contact@shaac.me>
2019-10-24 19:45:20 +0200

    check_code: parallel, and correct exit code

--- check_code.sh
+++ check_code.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-yapf --recursive -d iorgen/ test/test.py test/regenerate.py
+yapf -p --recursive -d iorgen/ test/test.py test/regenerate.py
+r1=$?
 mypy --strict iorgen test/test.py test/regenerate.py
-pylint -d R0801 iorgen test/test.py test/regenerate.py
+r2=$?
+pylint -j 0 -d R0801 iorgen test/test.py test/regenerate.py
+r3=$?
+[ $r1 -eq 0 ] && [ $r2 -eq 0 ] && [ $r3 -eq 0 ]
