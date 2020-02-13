0782f62470b5013b533f3a2e66d859e8839440ac
Sacha Delanoue <contact@shaac.me>
2020-02-13 22:09:47 +0100

    pylint: disable bad-contiation warning
    There already is a formatter for that

--- check_code.sh
+++ check_code.sh
@@ -4,6 +4,6 @@ yapf -p --recursive -d iorgen/ test/test.py test/regenerate.py
 r1=$?
 mypy --strict iorgen # test/test.py test/regenerate.py
 r2=$?
-pylint -j 0 -d R0801 iorgen test/test.py test/regenerate.py
+pylint -j 0 -d R0801 -d C0330 iorgen test/test.py test/regenerate.py
 r3=$?
 [ $r1 -eq 0 ] && [ $r2 -eq 0 ] && [ $r3 -eq 0 ]
